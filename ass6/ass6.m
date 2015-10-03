402b6d97b0e4e042f2a8e7be0bb298baa2c804b7
Andrea Jemmett <andreajemmett@gmail.com>
2015-10-03 10:06:58 +0200

    finally display lattice correctly!

--- ass6/ass6.m
+++ ass6/ass6.m
@@ -11,7 +11,8 @@ title('Generated dataset')
 
 nin = 2;
 map_sides = [2:10 11:2:21];
-map_size = [32, 32];
+map_size = [16, 16];
+options(1) = 0;     % Don't show iteration info
 options(5) = 1;     % Random sampled
 options(14) = 400;  % # of iterations 
 options(15) = 1;    % Final neighborhood size
@@ -19,15 +20,20 @@ options(16) = 0.05;  % Final learning rate
 options(17) = round(sqrt(map_size(1,2)^2 + map_size(1,1)^2)/2); % initial neighborhood size
 options(18) = 0.5;  % Initial learning rate
 
-disp('Doing SOM with square map of side 32')
+disp('Doing SOM with square map of side 16')
 net1 = som(nin, map_size);
+% c1 = sompak(net1);
+map1 = net1.map;
 subplot(1, 3, 2)
-plot(c1(:, 1), c1(:, 2), '-o')
+% plot(c1(:, 1), c1(:, 2), '-o')
+plot_lattice(map1, star)
 title(['SOM before training _{map\_size=[', num2str(map_size(1)), ', ', num2str(map_size(2)), ']}'])
 net2 = somtrain(net1, options, star);
-c2 = sompak(net2);
+% c2 = sompak(net2);
+map2 = net2.map;
 subplot(1, 3, 3)
-plot(c2(:, 1), c2(:, 2), '-o')
+% plot(c2(:, 1), c2(:, 2), '-o')
+plot_lattice(map2, star)
 title(['SOM after training _{map\_size=[', num2str(map_size(1)), ', ', num2str(map_size(2)), ']}'])
 
 errors = [map_sides', zeros(length(map_sides), 1)];
@@ -43,9 +49,11 @@ for i = 1:length(map_sides);
     net = som(nin, map_size);
     net = somtrain(net, options, star);
     c = sompak(net);
+    map = net.map;
     
     subplot(length(map_sides) / 3, 3, i)
-    plot(c(:, 1), c(:, 2), '-o')
+    % plot(c(:, 1), c(:, 2), '-o')
+    plot_lattice(map, star)
     title(['map\_size = [', num2str(map_size(1,1)), ', ', num2str(map_size(1,2)), ']'])
     
     for j = 1:size(c, 1);
@@ -58,3 +66,5 @@ end
 
 figure
 plot(errors(:, 1), errors(:, 2))
+xlabel('Square map size')
+ylabel('Neurons inside training star shape')
---
+++ ass6/plot_lattice.m
@@ -0,0 +1,22 @@
+function plot_lattice( map, training )
+
+hold on
+plot(training(:, 1), training(:, 2), 'o')
+
+[~, mapw, maph] = size(map);
+
+for i = 1:mapw-1;
+    for j = 1:maph-1;
+        scatter([map(1, i, j) map(1, i+1, j)], [map(2, i, j) map(2, i+1, j)], 'MarkerFaceColor', 'y')
+        scatter([map(1, i, j) map(1, i, j+1)], [map(2, i, j) map(2, i, j+1)], 'MarkerFaceColor', 'y')
+        
+        
+        plot([map(1, i, j) map(1, i+1, j)], [map(2, i, j) map(2, i+1, j)], 'r-')
+        plot([map(1, i, j) map(1, i, j+1)], [map(2, i, j) map(2, i, j+1)], 'r-')
+    end
+end
+
+hold off
+
+end
+
