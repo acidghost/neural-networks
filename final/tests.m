1944d663fa1174f744c696ec60719a1b23c3a3f5
Andrea Jemmett <andreajemmett@gmail.com>
2015-10-13 10:44:17 +0200

    try SVM on all data

--- final/tests.m
+++ final/tests.m
@@ -20,27 +20,58 @@ end
 % sources of panic (e.g. shouting individual)
 source = [542.0, max_y-439.0];
 
-% draw person 'a'
-a = 8;
-hold on
-grid on
-axis([0 max_x+1 0 max_y+1])
-path = zeros(end_time, 2);
-for i = 1:end_time;
-    plot(data(i,(2*a)-1),data(i,(2*a)),'Color',[0 0 1],'Marker','.','MarkerSize',10);
-    path(i, :) = [data(i, (2*a)-1), data(i, 2*a)];
+% % draw person 'a'
+% a = 8;
+% hold on
+% grid on
+% axis([0 max_x+1 0 max_y+1])
+% path = zeros(end_time, 2);
+% for i = 1:end_time;
+%     plot(data(i,(2*a)-1),data(i,(2*a)),'Color',[0 0 1],'Marker','.','MarkerSize',10);
+%     path(i, :) = [data(i, (2*a)-1), data(i, 2*a)];
+% end
+% 
+% figure
+% plot(path(:, 1), path(:, 2))
+% 
+% Xpath = path(1:end_time-1, :);
+% Xtra = [Xpath pdist2(Xpath, source)];
+% Ytra = path(2:end_time, :);
+% model = initlssvm( Xtra, Ytra, 'f', [], [], 'RBF_kernel' );
+% model = tunelssvm(model, 'simplex', 'crossvalidatelssvm', { 10, 'mae' });
+% model = trainlssvm(model);
+% Yhs = simlssvm(model, Xtra);
+% 
+% error = diag(pdist2(Yhs, Ytra));
+% disp(['Error is ', num2str(sum(error))])
+
+Xpaths = zeros((end_time-1) * number_of_agents, 2);
+Ypaths = zeros((end_time-1) * number_of_agents, 2);
+Xcounter = 1;
+Ycounter = 1;
+for agent = 1:number_of_agents;
+    for i = 1:end_time;
+        if i == 1
+            Xpaths(Xcounter, :) = [data(i, (2*a)-1), data(i, 2*a)];
+            Xcounter = Xcounter + 1;
+        elseif i == end_time;
+            Ypaths(Ycounter, :) = [data(i, (2*a)-1), data(i, 2*a)];
+            Ycounter = Ycounter + 1;
+        else
+            Xpaths(Xcounter, :) = [data(i, (2*a)-1), data(i, 2*a)];
+            Xcounter = Xcounter + 1;
+            Ypaths(Ycounter, :) = [data(i, (2*a)-1), data(i, 2*a)];
+            Ycounter = Ycounter + 1;
+        end
+    end
 end
 
-figure
-plot(path(:, 1), path(:, 2))
-
-Xpath = path(1:end_time-1, :);
-Xtra = [Xpath pdist2(Xpath, source)];
-Ytra = path(2:end_time, :);
-model = initlssvm( Xtra, Ytra, 'f', [], [], 'RBF_kernel' );
+Xtra = [Xpaths pdist2(Xpaths, source)];
+model = initlssvm( Xtra, Ypaths, 'f', [], [], 'RBF_kernel' );
 model = tunelssvm(model, 'simplex', 'crossvalidatelssvm', { 10, 'mae' });
 model = trainlssvm(model);
 Yhs = simlssvm(model, Xtra);
 
-error = diag(pdist2(Yhs, Ytra));
-disp(['Error is ', num2str(sum(error))])
+error = diag(pdist2(Yhs, Ypaths));
+MSE = mean(sqrt(error));
+disp(['MSE is ', num2str(MSE)])
